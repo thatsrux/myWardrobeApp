91b7f99ef476c3d8a52b386625911ead7c078ab8
Vincenzo Russo <vincenzom121@gmail.com>
2024-07-12 17:51:45 +0200

    separation of responsibility

--- AddClothScreen.swift
+++ AddClothScreen.swift
@@ -1,52 +1,5 @@
-//
-//  AddClothScreen.swift
-//  myWardrobe
-//
-//  Created by Studente on 02/07/24.
-//
 import ColorKit
 import SwiftUI
-
-import UIImageColors
-import BackgroundRemoval
-
-
-
-
-class Cloth {
-    let image: UIImage
-    let imageNoBackground: UIImage
-    
-    var mainColor: Color
-    var secondColor: Color?
-    var thirdColor: Color?
-    
-    var categoria: String
-    var nome:String
-    var taglia:String
-    
-    let backgroundRemoval = BackgroundRemoval()
-    
-    init(image: UIImage) {
-        self.image = image
-        do {
-            self.imageNoBackground = try backgroundRemoval.removeBackground(image: image)
-        }catch {
-            fatalError(error.localizedDescription)
-        }
-        let colors = imageNoBackground.getColors()
-        mainColor = Color((colors?.background)!)
-        if let second = colors?.primary {
-            secondColor = Color(second)
-        }
-        if let third = colors?.secondary {
-            thirdColor = Color(third)
-        }
-        self.categoria = ""
-        self.nome = ""
-        self.taglia = ""
-    }
-}
 
 struct AddClothScreen: View {
     var cloth: Cloth
@@ -63,7 +16,6 @@ struct AddClothScreen: View {
     
     init(cloth: Cloth){
         self.cloth = cloth
-        self.categoriaText = ""
         self.nomeText = ""
         self.tagliaText = ""
         self.cpColor1 = Color(cloth.mainColor)
---
+++ Cloth.swift
@@ -0,0 +1,39 @@
+import ColorKit
+import SwiftUI
+import UIImageColors
+import BackgroundRemoval
+
+class Cloth {
+    let image: UIImage
+    let imageNoBackground: UIImage
+    
+    var mainColor: Color
+    var secondColor: Color?
+    var thirdColor: Color?
+    
+    var categoria: String
+    var nome:String
+    var taglia:String
+    
+    let backgroundRemoval = BackgroundRemoval()
+    
+    init(image: UIImage) {
+        self.image = image
+        do {
+            self.imageNoBackground = try backgroundRemoval.removeBackground(image: image)
+        }catch {
+            fatalError(error.localizedDescription)
+        }
+        let colors = imageNoBackground.getColors()
+        mainColor = Color((colors?.background)!)
+        if let second = colors?.primary {
+            secondColor = Color(second)
+        }
+        if let third = colors?.secondary {
+            thirdColor = Color(third)
+        }
+        self.categoria = ""
+        self.nome = ""
+        self.taglia = ""
+    }
+}
