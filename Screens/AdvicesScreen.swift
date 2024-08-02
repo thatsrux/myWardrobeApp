c9063418a73997a8025ea85f5581de1ea8601a99
Vincenzo Russo <vincenzom121@gmail.com>
2024-08-02 19:53:13 +0200

    folder fixings

--- Screens/AdvicesScreen.swift
+++ Screens/AdvicesScreen.swift
@@ -7,6 +7,8 @@ struct AdvicesScreen: View {
     @State private var dailyOutfitIndex: Int?
     @State private var isColorMatchScreenActive = false
     @State private var isColorSeasonScreenActive = false
+    @State private var isMagicOutfitScreenActive = false
+    
     var body: some View {
         NavigationStack {
             ScrollView {
@@ -37,6 +39,37 @@ struct AdvicesScreen: View {
                     .sheet(isPresented: $isColorMatchScreenActive) {
                         ColorMatch()
                     }
+                    
+                    Button(action: {
+                        isColorSeasonScreenActive.toggle()
+                    }) {
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color(.teal))
+                            .frame(height: 100)
+                            .overlay(
+                                Text("Color Season")
+                                    .font(.headline)
+                                    .foregroundColor(.white)
+                            )
+                    }.sheet(isPresented: $isColorSeasonScreenActive) {
+                        ColorSeason()
+                    }
+                    
+                    Button(action: {
+                        isMagicOutfitScreenActive.toggle()
+                    }) {
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color(.teal))
+                            .frame(height: 100)
+                            .overlay(
+                                Text("Magic Outfit")
+                                    .font(.headline)
+                                    .foregroundColor(.white)
+                            )
+                    }.sheet(isPresented: $isMagicOutfitScreenActive) {
+                        MagicOutfit()
+                    }
+                    
                     Button(action: {
                         isColorSeasonScreenActive.toggle()
                     }) {
@@ -129,138 +162,9 @@ struct AdvicesScreen: View {
     }
 }
 
-struct ColorMatch: View{
-    
-    var body: some View{
-        NavigationView {
-            ScrollView{
-                VStack(spacing:20){
-                    Text("Abbinamenti consentiti:")
-                    HStack(spacing: 40){
-                        Text("Rosso")
-                        Circle().fill(.red).frame(width: 60, height: 60).overlay(Circle().stroke(Color.black, lineWidth:0.5))
-                        Circle().fill(.blue).frame(width: 60, height: 60).overlay(Circle().stroke(Color.black, lineWidth:0.5))
-                        Text("Blu")
-                    }
-                    HStack(spacing: 40){
-                        Text("Giallo")
-                        Circle().fill(.yellow).frame(width: 60, height: 60).overlay(Circle().stroke(Color.black, lineWidth:0.5))
-                        Circle().fill(.green).frame(width: 60, height: 60).overlay(Circle().stroke(Color.black, lineWidth:0.5))
-                        Text("Verde")
-                    }
-                    HStack(spacing: 40){
-                        Text("Nero")
-                        Circle().fill(.black).frame(width: 60, height: 60).overlay(Circle().stroke(Color.black, lineWidth:0.5))
-                        Circle().fill(.gray).frame(width: 60, height: 60).overlay(Circle().stroke(Color.black, lineWidth:0.5))
-                        Text("Grigio")
-                    }
-                }
-                VStack(spacing:20){
-                    Text("Abbinamenti sconsigliati:")
-                    HStack(spacing: 40){
-                        Text("Rosso")
-                        Circle().fill(.red).frame(width: 60, height: 60).overlay(Circle().stroke(Color.black, lineWidth:0.5))
-                        Circle().fill(.blue).frame(width: 60, height: 60).overlay(Circle().stroke(Color.black, lineWidth:0.5))
-                        Text("Blu")
-                    }
-                    HStack(spacing: 40){
-                        Text("Giallo")
-                        Circle().fill(.yellow).frame(width: 60, height: 60).overlay(Circle().stroke(Color.black, lineWidth:0.5))
-                        Circle().fill(.green).frame(width: 60, height: 60).overlay(Circle().stroke(Color.black, lineWidth:0.5))
-                        Text("Verde")
-                    }
-                    HStack(spacing: 40){
-                        Text("Nero")
-                        Circle().fill(.black).frame(width: 60, height: 60).overlay(Circle().stroke(Color.black, lineWidth:0.5))
-                        Circle().fill(.gray).frame(width: 60, height: 60).overlay(Circle().stroke(Color.black, lineWidth:0.5))
-                        Text("Grigio")
-                    }
-                }
-                
-                .navigationTitle("Abbinamento colori")
-            }}
-    }
-}
 
-struct ColorSeason: View {
-    
-    @State private var showSpringColors = false
-    @State private var showSummerColors = false
-    @State private var showAutumnColors = false
-    @State private var showWinterColors = false
-    
-    var body: some View {
-        NavigationView {
-            ScrollView {
-                LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], spacing: 20) {
-                    SeasonSection(title: "Primavera", colors: [Color.green, Color.pink, Color.yellow], allColors: [Color.green, Color.pink, Color.yellow, Color.blue, Color.purple], isExpanded: $showSpringColors)
-                    
-                    SeasonSection(title: "Estate", colors: [Color.blue, Color.white, Color.cyan], allColors: [Color.blue, Color.white, Color.cyan, Color.orange, Color.gray], isExpanded: $showSummerColors)
-                    
-                    SeasonSection(title: "Autunno", colors: [Color.brown, Color.orange, Color.red], allColors: [Color.brown, Color.orange, Color.red, Color.yellow, Color.yellow], isExpanded: $showAutumnColors)
-                    
-                    SeasonSection(title: "Inverno", colors: [Color.black, Color.gray, Color.blue], allColors: [Color.black, Color.gray, Color.blue, Color.red, Color.purple], isExpanded: $showWinterColors)
-                }
-                .padding()
-            }
-            .navigationTitle("Color Season")
-        }
-    }
-}
 
-struct SeasonSection: View {
-    let title: String
-    let colors: [Color]
-    let allColors: [Color]
-    @Binding var isExpanded: Bool
-    
-    var body: some View {
-        VStack(alignment: .leading) {
-            Button(action: {
-                withAnimation {
-                    isExpanded.toggle()
-                }
-            }) {
-                HStack {
-                    Text(title)
-                        .font(.headline)
-                    Spacer()
-                    Image(systemName: isExpanded ? "chevron.up" : "chevron.down")
-                }
-                .padding()
-                .background(Color.gray.opacity(0.2))
-                .cornerRadius(10)
-            }
-            
-            if isExpanded {
-                ForEach(allColors, id: \.self) { color in
-                    Rectangle()
-                        .fill(color)
-                        .frame(height: 30)
-                        .cornerRadius(5)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 5)
-                                .stroke(Color.black, lineWidth: 1)
-                        )
-                        .padding(.leading)
-                }
-            } else {
-                ForEach(colors.prefix(3), id: \.self) { color in
-                    Rectangle()
-                        .fill(color)
-                        .frame(height: 30)
-                        .cornerRadius(5)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 5)
-                                .stroke(Color.black, lineWidth: 1)
-                        )
-                        .padding(.leading)
-                }
-            }
-        }
 
-    }
-}
 
 #Preview {
     AdvicesScreen().environmentObject(Database())
---
+++ Screens/AdvicesScreenSection/MagicOutfit.swift
@@ -0,0 +1,54 @@
+import SwiftUI
+
+struct MagicOutfit: View{
+
+    var body: some View{
+        NavigationView {
+            ScrollView{
+                VStack(spacing:20){
+                    Text("Abbinamenti consentiti:")
+                    HStack(spacing: 40){
+                        Text("Rosso")
+                        Circle().fill(.red).frame(width: 60, height: 60).overlay(Circle().stroke(Color.black, lineWidth:0.5))
+                        Circle().fill(.blue).frame(width: 60, height: 60).overlay(Circle().stroke(Color.black, lineWidth:0.5))
+                        Text("Blu")
+                    }
+                    HStack(spacing: 40){
+                        Text("Giallo")
+                        Circle().fill(.yellow).frame(width: 60, height: 60).overlay(Circle().stroke(Color.black, lineWidth:0.5))
+                        Circle().fill(.green).frame(width: 60, height: 60).overlay(Circle().stroke(Color.black, lineWidth:0.5))
+                        Text("Verde")
+                    }
+                    HStack(spacing: 40){
+                        Text("Nero")
+                        Circle().fill(.black).frame(width: 60, height: 60).overlay(Circle().stroke(Color.black, lineWidth:0.5))
+                        Circle().fill(.gray).frame(width: 60, height: 60).overlay(Circle().stroke(Color.black, lineWidth:0.5))
+                        Text("Grigio")
+                    }
+                }
+                VStack(spacing:20){
+                    Text("Abbinamenti sconsigliati:")
+                    HStack(spacing: 40){
+                        Text("Rosso")
+                        Circle().fill(.red).frame(width: 60, height: 60).overlay(Circle().stroke(Color.black, lineWidth:0.5))
+                        Circle().fill(.blue).frame(width: 60, height: 60).overlay(Circle().stroke(Color.black, lineWidth:0.5))
+                        Text("Blu")
+                    }
+                    HStack(spacing: 40){
+                        Text("Giallo")
+                        Circle().fill(.yellow).frame(width: 60, height: 60).overlay(Circle().stroke(Color.black, lineWidth:0.5))
+                        Circle().fill(.green).frame(width: 60, height: 60).overlay(Circle().stroke(Color.black, lineWidth:0.5))
+                        Text("Verde")
+                    }
+                    HStack(spacing: 40){
+                        Text("Nero")
+                        Circle().fill(.black).frame(width: 60, height: 60).overlay(Circle().stroke(Color.black, lineWidth:0.5))
+                        Circle().fill(.gray).frame(width: 60, height: 60).overlay(Circle().stroke(Color.black, lineWidth:0.5))
+                        Text("Grigio")
+                    }
+                }
+                
+                .navigationTitle("Abbinamento colori")
+            }}
+    }
+}
