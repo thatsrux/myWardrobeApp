0c24525c046b8fe76868f760a0faab52cfdaeb7e
Vincenzo Russo <vincenzom121@gmail.com>
2024-08-02 15:33:47 +0200

    colorMatch

--- Screens/AdvicesScreen.swift
+++ Screens/AdvicesScreen.swift
@@ -10,8 +10,130 @@ import ColorKit
 
 struct AdvicesScreen: View {
     
+<<<<<<< Updated upstream
     var body: some View {
         Text("")
+=======
+    @EnvironmentObject var database: Database
+    
+    @State private var dailyOutfitIndex: Int?
+    @State private var isColorMatchScreenActive = false
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                VStack {
+                    Text("Outfit del giorno").font(.headline)
+                    
+                    if let index = dailyOutfitIndex, !database.outfits.isEmpty {
+                        NavigationLink(destination: AddOutfitScreen(outfit: database.outfits[index])) {
+                            SingleOutfitGrid(outfit: database.outfits[index])
+                        }
+                    } else {
+                        Text("No outfit available").font(.subheadline).foregroundColor(.gray)
+                    }
+                }
+                LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], spacing: 20) {
+                    // Sostituzione del Button con un NavigationLink per aprire ColorMatch come un sheet
+                    Button(action: {
+                        isColorMatchScreenActive.toggle()
+                    }) {
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color.blue)
+                            .frame(height: 100)
+                            .overlay(
+                                Text("Abbinamento colori")
+                                    .font(.headline)
+                                    .foregroundColor(.white)
+                            )
+                    }
+                    .sheet(isPresented: $isColorMatchScreenActive) {
+                        ColorMatch()
+                    }
+                }
+                .padding()
+                
+                .navigationTitle("Consigli per te")
+                .onAppear {
+                    updateDailyOutfitIndexIfNeeded()
+                }
+            }
+        }
+    }
+    
+    private func updateDailyOutfitIndexIfNeeded() {
+        let today = Date()
+        let calendar = Calendar.current
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        
+        let todayString = dateFormatter.string(from: today)
+        
+        if let lastUpdateString = UserDefaults.standard.string(forKey: "lastUpdateDate"),
+           let lastUpdateDate = dateFormatter.date(from: lastUpdateString),
+           calendar.isDate(today, inSameDayAs: lastUpdateDate) {
+            // Same day, retrieve the stored outfit index
+            dailyOutfitIndex = UserDefaults.standard.integer(forKey: "dailyOutfitIndex")
+            //print("Retrieved dailyOutfitIndex: \(String(describing: dailyOutfitIndex))")
+        } else {
+            // New day, update the outfit index
+            if !database.outfits.isEmpty {
+                dailyOutfitIndex = Int.random(in: 0..<database.outfits.count)
+                UserDefaults.standard.set(todayString, forKey: "lastUpdateDate")
+                if let index = dailyOutfitIndex {
+                    UserDefaults.standard.set(index, forKey: "dailyOutfitIndex")
+                }
+                //print("Generated new dailyOutfitIndex: \(String(describing: dailyOutfitIndex))")
+            }
+        }
+    }
+    
+    struct SingleOutfitGrid: View {
+        
+        private var outfit: Outfit
+        
+        init(outfit: Outfit) {
+            self.outfit = outfit
+        }
+        
+        var body: some View {
+            HStack {
+                VStack(spacing: 10) {
+                    Image(uiImage: outfit.shirt?.image?.toImage() ?? UIImage(imageLiteralResourceName: "shirt"))
+                        .resizable()
+                        .scaledToFit()
+                        .clipped()
+                        .frame(width: 100, height: 100)
+                    Image(uiImage: outfit.trousers?.image?.toImage() ?? UIImage(imageLiteralResourceName: "trousers"))
+                        .resizable()
+                        .scaledToFit()
+                        .clipped()
+                        .frame(width: 100, height: 100)
+                    Image(uiImage: outfit.shoes?.image?.toImage() ?? UIImage(imageLiteralResourceName: "shoes"))
+                        .resizable()
+                        .scaledToFit()
+                        .clipped()
+                        .frame(width: 100, height: 100)
+                    Text(outfit.nome!)
+                        .foregroundStyle(.black)
+                }
+                .frame(width: 150, height: 370)
+                .background(Color.white)
+                .cornerRadius(10)
+                .shadow(radius: 5)
+                .padding(10)
+            }
+        }
+>>>>>>> Stashed changes
+    }
+}
+
+struct ColorMatch: View{
+    
+    var body: some View{
+        NavigationView {
+                    Text("Colori")
+                        .navigationTitle("Abbinamento colori")
+                }
     }
 }
 
